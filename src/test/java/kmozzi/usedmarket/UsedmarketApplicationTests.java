65b9fbc76d5f28eaacb149de03daf88340d74cb8
김경모 <kimgang@gimgyeongmoui-MacBookPro.local>
2023-12-11 17:18:55 +0900

    test commit

---
+++ src/test/java/kmozzi/usedmarket/UsedmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package kmozzi.usedmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
